bde169c58fcfb242c25c451f97343490e6fbcb83
ddh38796-lab <ddh38796@gmail.com>
2026-03-01 09:30:14 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,31 @@
+BOT_TOKEN = "8251459062:AAE2BYYJBeUzetKeXL6WnMQt4wZ2617WwQ8"
+ADMIN_ID = 8653235629
+CHANNEL_USERNAME = "@AmrDigitalChannel"
+PAYMENT_METHOD = "Vodafone Cash"
+MY_PHONE_NUMBER = "01066358683"
+DB_FILE = "amr_empire.db"
+MINIMUM_PRICE = 25
+PRICE_STEPS = [(20,25),(25,30),(30,35),(35,40),(40,45),(45,50),(50,55),(55,60),(60,65),(65,70),(70,75),(75,80),(80,85),(85,90),(90,95),(95,100),(100,105)]
+REFERRAL_BONUS = 5
+MIN_WITHDRAW = 1000
+FREE_SERVICE_LIMITS = {"like":10, "view":100, "default":10}
+APIS = {
+    "SMMPOWERMEDIA": {
+        "name": "🔥 SMM Power Media",
+        "url": "https://smmpowermadia.com/api/v2",
+        "key": "43167b99eafa62a1dcd4cd746a4e6dfe",
+        "type": "paid",
+        "phone": "01015430736",
+        "emoji": "🔥"
+    },
+    "SMMFANS": {
+        "name": "⚡ SMM Fans",
+        "url": "https://smmfansfaster.com/api/v2",
+        "key": "7798930d433943297672e0c1e328f2b4",
+        "type": "paid",
+        "phone": "01064271047",
+        "emoji": "⚡"
+    }
+}
+CATEGORIES = {"instagram":"📸 إنستجرام","youtube":"▶️ يوتيوب","facebook":"👤 فيسبوك","twitter":"🐦 تويتر","tiktok":"🎵 تيك توك","telegram":"✈️ تليجرام","spotify":"🎧 سبوتيفاي","soundcloud":"🎼 ساوند كلاود","other":"📦 أخرى"}
+SERVICES_PER_PAGE = 10
